0ac964c14c315f0a9eef5b02e154117c90ece8d1
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-06-22 16:01:48 +0530

    added find polyfill

--- src/polyfills/find.js
+++ src/polyfills/find.js
@@ -1,7 +1,33 @@
 // Note: Array.prototype.find does not skip sparse array unlike forEach, map, filter
 
-Array.prototype.findPolyfill = function (cb, thisArg) {};
+Array.prototype.findPolyfill = function (cb, thisArg) {
+  "use strict";
+
+  if (typeof cb !== "function") {
+    throw new TypeError(`${typeof cb} ${cb} is not a function`);
+  }
+
+  if (this === null || this === undefined) {
+    throw new TypeError(
+      "Array.prototype.findPolyfill called on null or undefined"
+    );
+  }
+
+  let i = 0;
+  let length = this.length;
+  let result = undefined;
+  while (i < length) {
+    const isTrue = cb.call(thisArg, this[i], i, this);
+    if (isTrue) {
+      result = this[i];
+      break;
+    }
+    i++;
+  }
+
+  return result;
+};
 
 module.exports = {
-  findPolyfill: Array.prototype.find,
+  findPolyfill: Array.prototype.findPolyfill,
 };
