0b91d337decf35767d3acfee15ce5d3f2314f518
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-06-26 00:06:26 +0530

    refactored

--- src/polyfills/reduce.js
+++ src/polyfills/reduce.js
@@ -34,3 +34,44 @@ Array.prototype.reducePolyfill = function (cb, initialValue) {
 module.exports = {
   reducePolyfill: Array.prototype.reducePolyfill,
 };
+
+// ChatGPT solution
+// Array.prototype.reducePolyfill = function (callback, initialValue) {
+//   "use strict";
+
+//   if (this === null || this === undefined) {
+//     throw new TypeError("Array.prototype.reduce called on null or undefined");
+//   }
+
+//   if (typeof callback !== "function") {
+//     throw new TypeError(callback + " is not a function");
+//   }
+
+//   const obj = Object(this); // Convert array-like to object
+//   const len = obj.length >>> 0; // Convert to Uint32
+//   let i = 0;
+//   let accumulator;
+
+//   // Determine if we have an initial value
+//   if (arguments.length >= 2) {
+//     accumulator = initialValue;
+//   } else {
+//     // Find the first defined value to use as initial
+//     while (i < len && !(i in obj)) {
+//       i++;
+//     }
+//     if (i >= len) {
+//       throw new TypeError("Reduce of empty array with no initial value");
+//     }
+//     accumulator = obj[i++];
+//   }
+
+//   // Iterate over remaining elements
+//   for (; i < len; i++) {
+//     if (i in obj) {
+//       accumulator = callback(accumulator, obj[i], i, obj);
+//     }
+//   }
+
+//   return accumulator;
+// };
