3705787c5b23c40306055a2dda5d230d50285df3
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-06-26 22:18:41 +0530

    working on bind - not completed yet

--- src/polyfills/functions/bind.js
+++ src/polyfills/functions/bind.js
@@ -1,5 +1,26 @@
-Function.prototype.bindPolyfill = function () {};
+Function.prototype.bindPolyfill = function (context, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError(
+      "Function.prototype.bindPolyfill can not call on null or undefined"
+    );
+  }
+
+  const orignalFn = this;
+  const ctx = Object.assign({}, context) || globalThis;
+  ctx.fn = this;
+  function boundFn(...newArgs) {
+    const result = ctx.fn(...args, ...newArgs);
+    delete ctx.fn;
+    return result;
+  }
+
+  if (orignalFn.prototype) {
+    boundFn.prototype = Object.create(orignalFn.prototype);
+  }
+
+  return boundFn;
+};
 
 module.exports = {
-  bindPolyfill: Function.prototype.bind,
+  bindPolyfill: Function.prototype.bindPolyfill,
 };
