3f253b761401d63cc0ceb1517fd9f001d08f0795
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-07-13 22:56:08 +0530

    working on call polyfill, not completed yet

--- src/polyfills/functions/call.js
+++ src/polyfills/functions/call.js
@@ -1,5 +1,18 @@
-Function.prototype.callPolyfill = function () {};
+Function.prototype.callPolyfill = function (thisArg, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError(
+      "Function.prototype.callPloyfill can only be called on functions"
+    );
+  }
+
+  let context = thisArg || globalThis;
+  const key = Symbol("temp_key");
+  context[key] = this;
+  const result = context[key](...args);
+  delete context[key];
+  return result;
+};
 
 module.exports = {
-  callPolyfill: Function.prototype.call,
+  callPolyfill: Function.prototype.callPolyfill,
 };
