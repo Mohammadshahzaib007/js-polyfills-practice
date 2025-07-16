bf32e309dd33d22fa8cef1920e848479291146b1
Mohammad_shahzaib07 <shahzaib@quolam.com>
2025-07-16 19:50:58 +0530

    call polyfill done

--- src/polyfills/functions/call.js
+++ src/polyfills/functions/call.js
@@ -6,6 +6,13 @@ Function.prototype.callPolyfill = function (thisArg, ...args) {
   }
 
   let context = thisArg || globalThis;
+
+  if (typeof thisArg === "number") {
+    context = new Number(thisArg);
+  }
+  if (typeof thisArg === "string") {
+    context = new String(thisArg);
+  }
   const key = Symbol("temp_key");
   context[key] = this;
   const result = context[key](...args);
